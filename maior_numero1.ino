d84c2fc0de1ac61694ee3a3d119f78a22b245b80
Joao <joaofeitosa029@gmail.com>
2025-05-09 16:46:39 -0300

    feito as atividades do dia 09/05/25

---
+++ maior_numero1.ino
@@ -0,0 +1,31 @@
+// C++ code
+//
+int num1;
+int num2;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  
+  Serial.println("Digite o primeiro numero:");
+  while (!Serial.available());
+  num1 = Serial.parseInt();
+
+  Serial.println("Digite o segundo numero:");
+  while (!Serial.available());
+  num2 = Serial.parseInt();
+
+  if (num1 > num2) {
+    Serial.print("O maior numero e: ");
+    Serial.println(num1);
+  } else {
+    Serial.print("O maior numero e: ");
+    Serial.println(num2);
+  
+  }
+
+  delay(3000);
+}
