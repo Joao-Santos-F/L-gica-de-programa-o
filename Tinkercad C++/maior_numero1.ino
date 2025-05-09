4d196cc087e6b321f3a81f95b83f202256d92553
Joao Victor <joaofeitosa029@gmail.com>
2025-05-09 13:00:48 -0300

    Update maior_numero1.ino

--- Tinkercad C++/maior_numero1.ino
+++ Tinkercad C++/maior_numero1.ino
@@ -1,29 +1,28 @@
 // C++ code
 //
+
+int num1;
+int num2;
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int num1;
-  int num2;
+  
   
   Serial.println("Digite o primeiro numero:");
   while (!Serial.available());
   num1 = Serial.parseInt();
-  while (Serial.available()) Serial.read();
 
   Serial.println("Digite o segundo numero:");
   while (!Serial.available());
   num2 = Serial.parseInt();
-  while (Serial.available()) Serial.read();
 
   if (num1 > num2) {
     Serial.print("O maior numero e: ");
     Serial.println(num1);
-  } else if (num2 > num1) {
-    Serial.print("O maior numero e: ");
-    Serial.println(num2);
+ 
   } else {
     Serial.println("Os dois numeros sao iguais.");
   }
